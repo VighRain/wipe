069f885df8e7412982aa0e534e1d09bc279aef59
Ilya Pavlichenko <vighrain@gmail.com>
2022-10-12 21:32:21 +0300

    Added file corruption function

--- wipe.py
+++ wipe.py
@@ -1,2 +1,20 @@
+import random
+
+
+def corruptFile(pathtowipe):
+    with open(pathtowipe, 'r') as f:
+        contents = f.read()
+        for i in range(0, len(contents)):
+            insert_at = random.randint(0, len(contents))
+            contents = contents[:insert_at] + '#' + contents[insert_at:]
+        f.close()
+    with open(pathtowipe, 'w') as f:
+        f.write(contents)
+        f.close()
+    return contents
+
+
 if __name__ == "__main__":
-    pass
+    print("File path:")
+    pathtowipe = input()
+    corruptFile(pathtowipe)
