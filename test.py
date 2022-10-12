7a05dc36abd5c35288fec6ae9d80b7f88777d5ff
Ilya Pavlichenko <vighrain@gmail.com>
2022-10-12 21:22:16 +0300

    Added file corruption test

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 import unittest
 import wipe
 
+class testWipe(unittest.TestCase):
+
+    def test1WasFileCorrupted(self):
+        print("File corruption test")
+        pathtowipe = input()
+        with open(pathtowipe, 'r') as f:
+            contents = f.read()
+        corrupted_contents = wipe.corruptFile(pathtowipe)
+        self.assertNotEqual(contents, corrupted_contents)
+
+
 if __name__ == "__main__":
     unittest.main()
